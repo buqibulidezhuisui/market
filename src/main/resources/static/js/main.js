17ed30d98709764fa69506cbfeef1203b976c7f8
caoyong <1666416683@qq.com>
2019-01-15 13:24:48 +0800

    左侧菜单的点击事件

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,16 @@
-
 "use strict";
 //# sourceURL=main.js
 
 // DOM 加载完再执行
 $(function() {
-    location.href = "/users/search"
+    // 菜单事件
+    $(".blog-menu .list-group-item").click(function() {
+        var url = $(this).attr("url");
+
+        // 先移除其他的点击样式，再添加当前的点击样式
+        $(".blog-menu .list-group-item").removeClass("active");
+        $(this).addClass("active");
+
+        location.href = url;
+    });
 });
